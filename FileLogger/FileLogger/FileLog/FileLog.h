7cb9428bb14f18710d3dd4d0444b3e831f6819bd
YenKaiHuang <jamison7216@gmail.com>
2014-10-16 11:54:24 +0800

    Add FileLog Object

---
+++ FileLogger/FileLogger/FileLog/FileLog.h
@@ -0,0 +1,13 @@
+//
+//  FileLog.h
+//  FileLogger
+//
+//  Created by yenkai huang on 2014/10/16.
+//  Copyright (c) 2014年 yenkai huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileLog : NSObject
+
+@end
---
+++ FileLogger/FileLogger/FileLog/FileLog.m
@@ -0,0 +1,13 @@
+//
+//  FileLog.m
+//  FileLogger
+//
+//  Created by yenkai huang on 2014/10/16.
+//  Copyright (c) 2014年 yenkai huang. All rights reserved.
+//
+
+#import "FileLog.h"
+
+@implementation FileLog
+
+@end
