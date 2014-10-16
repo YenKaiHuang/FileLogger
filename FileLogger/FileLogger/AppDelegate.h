c8743b43ae787d8f19434c92c99fc4eba9f71d96
YenKaiHuang <jamison7216@gmail.com>
2014-10-16 09:45:05 +0800

    first commit

---
+++ FileLogger/FileLogger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FileLogger
+//
+//  Created by yenkai huang on 2014/10/16.
+//  Copyright (c) 2014年 yenkai huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FileLogger/FileLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FileLogger
+//
+//  Created by yenkai huang on 2014/10/16.
+//  Copyright (c) 2014年 yenkai huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
